560fb00c153131c4aca1758df198aa37615135da
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-12-22 15:49:58 +0900

    Defined "pid" to expand path

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -5,3 +5,5 @@ $timeout = 30
 $app_dir = "/var/www/relux-japan/current"
 
 $listen = File.expand_path 'tmp/sockets/unicorn.sock', $app_dir
+
+$pid = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
