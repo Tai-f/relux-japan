e9a2f784d8bec7aa0ad188c57ca63aa420328256
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-12-22 13:18:40 +0900

    Configure the linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :deploy_to, "/var/www/relux-japan"
 set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
