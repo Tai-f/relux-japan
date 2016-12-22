cc8a47493634fde24adef39be5b9657ee9bbce19
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-12-22 16:00:13 +0900

    Configure to apply each method

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -7,3 +7,14 @@ $app_dir = "/var/www/relux-japan/current"
 $listen = File.expand_path 'tmp/sockets/unicorn.sock', $app_dir
 
 $pid = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+
+$std_log = File.expand_path 'log/unicorn.log', $app_dir
+
+worker_processes $worker
+working_directory $app_dir
+stderr_path $std_log
+stdout_path $std_log
+timeout $timeout
+listen $listen
+pid $pid
+
