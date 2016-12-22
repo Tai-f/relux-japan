a06c64913dfd5efc40294d1ae675005afd9435fd
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-12-22 17:10:12 +0900

    Defined the method before fork do

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -20,4 +20,13 @@ pid $pid
 
 preload_app true
 
-
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      Process.kill "QUIT", File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
