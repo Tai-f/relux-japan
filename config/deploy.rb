e4173e6f199847a26822dae71c2e795eaa1806ae
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-12-22 13:18:17 +0900

    Configure the linked_files

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, "/var/www/relux-japan"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
-
+set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
