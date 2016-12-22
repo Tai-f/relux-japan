b6ceaadab78083b4a58dae3c19c2125a23e76e21
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-12-22 14:31:53 +0900

    Define the task as environment
    set unicorn_pid and unicorn_config

--- lib/capistrano/tasks/unicorn.rb
+++ lib/capistrano/tasks/unicorn.rb
@@ -1,2 +1,6 @@
 namespace :unicorn do
+  task :environment do
+    set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
+    set :unicorn_config, "#{current_path}/config/unicorn/production.rb"
+  end
 end
