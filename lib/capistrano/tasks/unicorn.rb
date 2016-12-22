bfc91ae59cf7e6d8f22c0e0ef4fd014b3af19617
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-12-22 14:34:30 +0900

    Define method "start_unicorn"
    to start unicorn same as terminal action

--- lib/capistrano/tasks/unicorn.rb
+++ lib/capistrano/tasks/unicorn.rb
@@ -3,4 +3,10 @@ namespace :unicorn do
     set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
     set :unicorn_config, "#{current_path}/config/unicorn/production.rb"
   end
+
+  def start_unicorn
+    within current_path do
+      execute :bundle, :exec, :unicorn, "-c #{fetch(:unicorn_config)} -E #{fetch(:rails_env)} -D"
+    end
+  end
 end
