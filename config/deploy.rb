4129b5b5d4a476d75492e0b69200e987c80983f7
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-12-22 14:25:31 +0900

    Describe 'create database'
    task defined as ‘db_create’

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,4 +39,17 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
+      end
+    end
+  end
+
+  
 end
