64537b8642d958843c649fb07d367a3c11ba8cc5
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2016-12-22 12:53:10 +0900

    Describe the server IP, username, the role of the server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,9 +49,9 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
+server "52.198.117.22",
+user: "tai",
+roles: %w{web app db}
 #   ssh_options: {
 #     user: "user_name", # overrides user setting above
 #     keys: %w(/home/user_name/.ssh/id_rsa),
