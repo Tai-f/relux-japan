0c5cf52f58e11d5930edf7c703d8babad1dc82cf
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2017-01-07 16:29:36 +0900

    Add action of "courses" routing
    it added index, new, create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :hotels, only: :index
-  resources :courses, only: :show
+  resources :courses, only: [:index,:new, :create, :show]
 
   root to: 'hotels#index'
 
