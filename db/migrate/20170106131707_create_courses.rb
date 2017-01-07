34539e18d8c4acafb39bdb995352b668f7c8871b
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2017-01-07 16:17:23 +0900

    Create "courses" table

--- db/migrate/20170106131707_create_courses.rb
+++ db/migrate/20170106131707_create_courses.rb
@@ -1,7 +1,11 @@
 class CreateCourses < ActiveRecord::Migration[5.0]
   def change
     create_table :courses do |t|
-
+      t.string :name, null:false
+      t.string :kanji_name, null:false
+      t.string :theme, null:false
+      t.integer :price, null:false
+      t.text :body, null:false
       t.timestamps
     end
   end
