32b43435abad13e7be15e099e74dd061c7c9d639
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2017-01-07 17:00:21 +0900

    Add "brand story" action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :hotels, only: :index do
     collection do
       get :service
+      get :brandstory
     end
   end
   resources :courses, only: [:index,:new, :create, :show]
