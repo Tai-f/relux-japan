54ae49fa98e3a99dbb8f8a9e5c449b979d1d3569
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2017-01-13 20:15:16 +0900

    It configured directory and asset_host of each env.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,4 +7,17 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
+  case Rails.env
+    when 'production'
+      config.fog_directory = 'relux-japan'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/relux-japan'
+
+    when 'development'
+      config.fog_directory = 'relux-japan-env'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/relux-japan-env'
+
+    when 'test'
+      config.storage = :file
+  end
+
 end
