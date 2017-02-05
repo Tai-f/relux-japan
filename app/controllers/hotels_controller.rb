d6d8ebe10e7a404293d666bc261042056982083d
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2017-02-05 21:00:11 +0900

    Define "show" method

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -5,9 +5,16 @@ class HotelsController < ApplicationController
   end
 
   def show
+    @hotel = Hotel.includes([:reviews, :rooms, :photos]). find(params[:id])
+    @photos = @hotel.photos
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def service
+    @hotels = Hotel.order("id DESC")
   end
 
   def brandstory
