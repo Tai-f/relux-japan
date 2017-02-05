ee61eaa59da8c04a1b997fc5e2a8c57522de9297
藤田泰史郎 <music-t.s.o@hotmail.co.jp>
2017-02-05 20:59:06 +0900

    make association with "room", "review" model

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -2,5 +2,7 @@ class Hotel < ApplicationRecord
   has_many :selections, through: :hotel_selections
   has_many :hotel_selections
   has_many :photos
+  has_many :rooms
+  has_many :reviews
   mount_uploader :image, ImageUploader
 end
